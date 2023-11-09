2280a412bdca966e7328899cf9a24769df43f7da
Sami Krim <samikrim0902@gmail.com>
2023-11-09 16:04:01 +0100

    Changed
    Writing errors correction

--- src/up/mi/skdh/exceptions/AccessibilityConstraintNotVerifiedException.java
+++ src/up/mi/skdh/exceptions/AccessibilityConstraintNotVerifiedException.java
@@ -2,6 +2,6 @@ package up.mi.skdh.exceptions;
 
 public class AccessibilityConstraintNotVerifiedException extends Exception {
 	public AccessibilityConstraintNotVerifiedException() {
-		super("Cas impossible après suppression impossible cat la contrainte d'accessibilité n'est pas respectée (il existe une divile qui n'a pas d'accès direct à une bore de chargement).");
+		super("Cas impossible après suppression car la contrainte d'accessibilité n'est pas respectée (il existe une divile qui n'a pas d'accès direct à une bore de chargement).");
 	}
 }
